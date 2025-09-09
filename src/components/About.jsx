8ea6f2eb7e07d98e77c65a903ae04af2cc9726e3
izaac08 <izaacmottiar46@gmail.com>
2025-09-08 21:07:08 -0400

    edited about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,11 @@ export default function About() {
   return (
     <section style={{marginTop: '2rem,'}} id="about" className="about-section">
       <h2 style={{marginBottom: '0.5rem'}}>About Me</h2>
-      <p>I'm currently in my third year of Mechatronics Engineering at the University of Waterloo, specializing in embedded systems and software development. I enjoy designing and validating hardware-software systems, with a particular interest in automation and data acquisition. I’m always looking for opportunities to build impactful, hands-on projects that stretch my skill set. In my spare time, I like to explore the outdoors, play intramural sports, and work on personal tech projects.
+      <p>I'm currently in my third year of Mechatronics Engineering at the University of Waterloo, 
+      specializing in electrical systems. I enjoy designing and validating hardware-software systems, 
+      with a particular interest in automation and data acquisition. I’m always looking for opportunities to build impactful, 
+      hands-on projects that stretch my skill set. In my spare time, I like to explore the outdoors, 
+      play intramural sports, and work on personal tech projects.
       </p>
     </section>
   );
